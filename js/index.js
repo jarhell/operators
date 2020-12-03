6485a37cb96a8e24217cd3937b3e124ab27991bc
jarhell <marihonja@gmail.com>
2020-12-03 19:43:06 +0300

    add operator assignment on the js folder to learn about thier functionality

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var greetings= "hello world!";
+console.log(greetings);
+//search() method
+var dressTosearch = "please share with me shire is located!"
+console.log(dressTosearch.search('shire'));
+//slice() method
+var fruitList= "mango, orange, melon, ovacado";
+console.log(fruitList.slice(7, 14));
+//assignment operators /string
+var favouriteDrink = "green tea";
+console.log(favouriteDrink);
+//number
+var favouriteNumber = 10;
+console.log(favouriteNumber += 5);
+//console.log(favouriteNumber);
+//operators assignment
+console.log(favouriteNumber -= 12);
+console.log(favouriteNumber *=12);
+console.log(favouriteNumber /=6);
+//comparison operators
+let myNumber = 12;
+console.log(myNumber ===12,);
+console.log(myNumber > 13);
+console.log(myNumber /2);
+console.log(myNumber % 1);
+console.log(myNumber += 25);
